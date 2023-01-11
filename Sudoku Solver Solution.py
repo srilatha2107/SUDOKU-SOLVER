41cbf29533c0fb450fcc495e84353fdade7969c0
srilatha2107 <82536726+srilatha2107@users.noreply.github.com>
2023-01-11 22:28:26 +0530

    Add files via upload

---
+++ Sudoku Solver Solution.py
@@ -0,0 +1,34 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\Srilatha\Desktop\Sudoko solver\Ssolver.py =========
+[[2 1 4 5 7 9 3 8 6]
+ [7 9 8 1 6 3 2 5 4]
+ [4 7 5 2 3 8 6 1 9]
+ [1 8 2 3 4 5 9 6 7]
+ [6 3 9 7 5 4 8 2 1]
+ [5 2 6 8 9 1 4 7 3]
+ [8 6 3 4 2 7 1 9 5]
+ [3 5 1 9 8 6 7 4 2]
+ [9 4 7 6 1 2 5 3 8]]
+more possible solutions
+[[2 1 4 5 7 9 6 8 3]
+ [7 9 8 1 6 2 3 5 4]
+ [6 7 5 4 3 8 2 1 9]
+ [4 8 1 3 2 5 9 7 6]
+ [9 3 2 7 5 4 8 6 1]
+ [5 2 6 8 9 1 4 3 7]
+ [8 6 3 2 4 7 1 9 5]
+ [1 5 9 6 8 3 7 4 2]
+ [3 4 7 9 1 6 5 2 8]]
+more possible solutions
+[[2 1 4 5 7 9 6 8 3]
+ [7 9 8 1 6 2 3 5 4]
+ [6 7 5 4 3 8 2 1 9]
+ [4 8 1 3 2 5 9 7 6]
+ [9 3 2 7 5 4 8 6 1]
+ [5 2 6 8 9 1 4 3 7]
+ [8 6 3 9 4 7 1 2 5]
+ [1 5 9 6 8 3 7 4 2]
+ [3 4 7 2 1 6 5 9 8]]
+more possible solutions
